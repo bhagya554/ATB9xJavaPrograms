fad97f934b2367a9325f8b334aaa6da1680018e6
Bhagya54 <139695549+Bhagya54@users.noreply.github.com>
2024-10-23 13:36:23 +0530

    Oct 17 2024

--- src/Oct/ex_17102024/Task001_GradeCalculator.java
+++ src/Oct/ex_17102024/Task001_GradeCalculator.java
@@ -40,4 +40,26 @@ public class Task001_GradeCalculator {
             System.out.println("Invalid input");
         }
     }
+
+    public static class Task002_TriangleClassification {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter side1:");
+            int side1=sc.nextInt();
+            System.out.println("Enter side2:");
+            int side2=sc.nextInt();
+            System.out.println("Enter side3:");
+            int side3=sc.nextInt();
+
+            if(side1==side2 && side1==side3){
+                System.out.println("equilateral Triangle");
+            }
+            else if(side1==side2 ||side1==side3 || side2==side3){
+                System.out.println("isosceles triangle");
+            }
+            else{
+                System.out.println("Scalene triangle");
+            }
+        }
+    }
 }
