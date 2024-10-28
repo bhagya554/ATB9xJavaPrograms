10176f16e5df6004600b2598ef897b53ef2d5572
Bhagya54 <testingtrainer54@gmail.com>
2024-10-28 10:15:54 +0530

    28Oct2024_more

--- src/Oct/ex_28102024/Lab001_StringFunctions.java
+++ src/Oct/ex_28102024/Lab001_StringFunctions.java
@@ -3,6 +3,7 @@ package Oct.ex_28102024;
 public class Lab001_StringFunctions {
     public static void main(String[] args) {
         String str1 = "Hello My Dear Selenium";
+        String str3 = "Hello my dear SelEnium";
         System.out.println(str1.charAt(6));//M
         System.out.println(str1.concat(" with Java"));//Hello My Dear Selenium with Java
         System.out.println(str1.replace('e', 'Z'));//HZllo My DZar SZlZnium
@@ -12,6 +13,15 @@ public class Lab001_StringFunctions {
         System.out.println(str1.startsWith("Hello"));//true
         System.out.println(str1.endsWith("nium"));//true
         System.out.println(str1.contains("Dear"));//true
+        System.out.println(str1.length());//22
+        System.out.println(str1.toLowerCase());//hello my dear selenium
+        System.out.println(str1.toUpperCase());//HELLO MY DEAR SELENIUM
+        System.out.println(str1.equals(str3));//false
+        System.out.println(str1.equalsIgnoreCase(str3));//true
+        String[] splitAfter=str1.split(" ");
+        for(int i=0;i<splitAfter.length;i++){
+            System.out.println(splitAfter[i]);
+        }
 
         String str2 = "   hellooo223 34Java 8989bye   ";
         System.out.println(str2.trim());//hellooo223 34Java 8989bye
